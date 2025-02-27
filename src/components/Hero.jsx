6ae280e2879b4cf48e41e225f50b4cb52b20b141
Arnavongit <arnavtrivediofficial@gmail.com>
2025-02-28 05:24:59 +0530

    Navbar Component Functional

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import profile_pic from "../assets/assets/profile_pic.jpg";
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <img src={profile_pic} width={200} height={200} />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa6";
+import { FiMail } from "react-icons/fi";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="flex justify-between items-center mb-15 py-6">
+        <div className="flex flex-shrink-0 items-center text-3xl text-white">
+          Arnav Trivedi
+        </div>
+
+        <div className="m-8 flex items-center justify-between gap-4 text-2xl">
+          <a
+            href="https://linkedin.com/in/arnavtrivedi2004"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="hover:text-slate-500 transition-colors duration-200" />
+          </a>
+
+          <a
+            href="https://github.com/4rnav-here"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub className="hover:text-slate-500 transition-colors duration-200" />
+          </a>
+
+          <a href="mailto:arnavtrivediofficial@gmail.com">
+            <FiMail className="hover:text-slate-500 transition-colors duration-200" />
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
